8b81aa1507f7927cac1bcc34aad895af2db2b896
hemantmehra <mehra.hemant14@gmail.com>
2018-07-19 10:32:51 +0530

    Add async comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @link, notice: 'Comment was successfully created.' }
+        format.js
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
